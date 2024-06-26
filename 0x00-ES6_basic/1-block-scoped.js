3343479254debca7701c2c4bc07d1fadf64be1f1
asenjialvin <asenjialvin@gmail.com>
2024-06-26 06:42:25 -0700

    Updated task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-	  const task = false;
-	  const task2 = true;
+  const task = false;
+  const task2 = true;
 
-	  if (trueOrFalse) {
-		      const task = true; // eslint-disable-line no-unused-vars
-		      const task2 = false; // eslint-disable-line no-unused-vars
-		    }
+  if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    const task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    const task2 = false;
+  }
 
-	  return [task, task2];
+  return [task, task2];
 }
