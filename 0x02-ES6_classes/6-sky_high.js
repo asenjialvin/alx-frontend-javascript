d2a1b34d76b257fc87c7e4f46ebaf0d458d0ed50
asenjialvin <asenjialvin@gmail.com>
2024-06-28 11:22:22 -0700

    Updated tasks 5 & 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,27 +2,16 @@ import Building from './5-building';
 
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (typeof floors !== 'number') {
-      throw new Error('Inavlid input');
-    }
     super(sqft);
     this._floors = floors;
   }
 
   get floors() {
-    return this._floors;
-  }
-
-  set floors(value) {
-    if (typeof value !== 'number') {
-      throw new Error('Floors must be a number');
-    } else {
-      this._floors = value;
-    }
+    return (this._floors);
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
 
